08414346802a2599a67e0cd7d028227aa82621ae
Mufaddal Jiruwala <56622301+mufaddal-ux@users.noreply.github.com>
2022-02-20 13:27:42 +0530

    AI Radio

---
+++ lib/utils/aiUtil.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
+// @HttpStatus.INTERNAL_SERVER_ERROR
+
+mixin AIColors {
+  static Color primaryColor1 = Vx.orange500;
+  static Color primaryColor2 = Vx.purple500;
+}
